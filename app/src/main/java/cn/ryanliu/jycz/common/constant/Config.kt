fd3bd58385cea764daa95ce6d6e331a455636b57
小刘宝贝 <996822526@qq.com>
2023-10-07 17:36:03 +0800

    添加车牌号输入框

--- app/src/main/java/cn/ryanliu/jycz/common/constant/Config.kt
+++ app/src/main/java/cn/ryanliu/jycz/common/constant/Config.kt
@@ -2,32 +2,12 @@ package cn.ryanliu.jycz.common.constant
 
 
 object Config {
-//    @JvmField
-//    var sPlatformServerIP = "jms.bcsyt.cn"//服务器
-//
-//    @JvmField
-//    var sPlatformServerPort = "33251"
-//
-//    @JvmField
-//    var sBaseUrl: String = "https://$sPlatformServerIP:$sPlatformServerPort"//服务器
+    @JvmField
+    var sPlatformServerIP = "118.186.19.35"//服务器
 
     @JvmField
-    var sPlatformServerIP = "jys.caacpl.com"//服务器
-
-    @JvmField
-    var sPlatformServerPort = "33254"
+    var sPlatformServerPort = "33254"   //测试 33254   正式 33251
 
     @JvmField
     var sBaseUrl: String = "http://$sPlatformServerIP:$sPlatformServerPort"//服务器
-
-//    @JvmField
-//    var sPlatformServerIP = "118.186.19.35"//服务器
-//
-//    @JvmField
-//    var sPlatformServerPort = "33251"
-//
-//    @JvmField
-//    var sBaseUrl: String = "http://$sPlatformServerIP:$sPlatformServerPort"//服务器
-
-
 }
